b245b0d1eba29f688af44277fba93da5801407fe
Mohammed Alkaabi <alkaabi@outlook.com>
2020-05-11 10:42:55 +0400

    updated the code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { createStore } from 'devextreme-aspnet-data-nojquery';
+import CustomStore from 'devextreme/data/custom_store';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
+
 export class AppComponent {
-  title = 'gridtest';
+  store: CustomStore;
+  constructor() {
+      let serviceUrl = "http://localhost:3005";
+      this.store = createStore({
+          key: "orderId",
+          loadUrl: serviceUrl + "/nwind/orders",
+          insertUrl: serviceUrl + "/nwind/insert-order",
+          updateUrl: serviceUrl + "/nwind/update-order",
+          deleteUrl: serviceUrl + "/nwind/delete-order"
+      });
+  }
 }
